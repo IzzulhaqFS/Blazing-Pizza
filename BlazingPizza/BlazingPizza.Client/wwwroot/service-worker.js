be845a3ab08ac73239445ed6b9553b167657764d
Izzulhaq Fawwaz Syauqiy <57482751+IzzulhaqFS@users.noreply.github.com>
2023-03-19 16:41:24 +0700

    Update service-worker.js

--- BlazingPizza/BlazingPizza.Client/wwwroot/service-worker.js
+++ BlazingPizza/BlazingPizza.Client/wwwroot/service-worker.js
@@ -4,7 +4,13 @@ self.addEventListener('install', async event => {
 });
 
 self.addEventListener('fetch', event => {
-    // You can add custom logic here for controlling whether to use cached data if offline, etc.
-    // The following line opts out, so requests go directly to the network as usual.
-    return null;
+    const payload = event.data.json();
+    event.waitUntil(
+        self.registration.showNotification('Blazing Pizza', {
+            body: payload.message,
+            icon: 'img/icon-512.png',
+            vibrate: [100, 50, 100],
+            data: { url: payload.url }
+        })
+    );
 });
